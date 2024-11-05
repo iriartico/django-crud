338385751ab6f043783cfdcd09f7b2d1a1b79b21
Jose Iriarte <joseiriarte2102@gmail.com>
2024-11-05 10:40:23 -0400

    Creacion de Usuarios

---
+++ tasks/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"]
+                )
+                user.save()
+                return HttpResponse("User created successfully")
+            except:
+                return HttpResponse("User already exists")
+
+        return HttpResponse("Passwords do not match")
